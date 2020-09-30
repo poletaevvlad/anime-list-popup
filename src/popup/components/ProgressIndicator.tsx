a5abc5022958fdc34db84763c209bc1435cfbcec
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-09-30 11:13:47 +0300

    Impelemented ProgressIndicator component

---
+++ src/popup/components/ProgressIndicator.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const ProgressIndicator = () =>
+    <div className="progress-indicator">
+        <div className="tile tile-tl" />
+        <div className="tile tile-tr" />
+        <div className="tile tile-bl" />
+        <div className="tile tile-br" />
+    </div>
+
+export default ProgressIndicator
